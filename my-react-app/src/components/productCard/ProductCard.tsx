86a8b90b5a68ffa8250e06eee4bd86601c60225d
http-sallein <sallein.caasi@gmail.com>
2025-02-12 18:32:31 -0300

    teste user

--- my-react-app/src/components/productCard/ProductCard.tsx
+++ my-react-app/src/components/productCard/ProductCard.tsx
@@ -2,7 +2,7 @@ export const ProductCard: React.FC = () => {
 
     return (
         <>
-        <div> Produto informations DevSaLLein </div>
+        <div> Produto informations testw 1 </div>
         </>
     )
 }
